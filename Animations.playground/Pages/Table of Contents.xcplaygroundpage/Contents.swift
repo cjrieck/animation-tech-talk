ffc8df37a40242a44427f6e43b477b87a34052af
Clayton Rieck <clayton.rieck@soundcloud.com>
2016-12-14 10:18:49 -0500

    rename the playground

--- Animations.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
+++ Animations.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 # Table of Contents
 
 * [Boring Animations](Basic%20Animations)
-* [Interesting Animations](Spring)
+* [Interesting Animations](Spring%20Animations)
 * [Gen Z Animations](Property%20Animator)
 * [More Gen Z Features for Animations](Some%20More%20Property%20Animator%20Features)
 * [UIKit Dynamics](Dynamics)

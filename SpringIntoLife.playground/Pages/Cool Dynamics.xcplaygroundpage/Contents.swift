c8f98769b664d6a97c262164fd5d7a6a7e75c3d1
Clayton Rieck <clayton.rieck@soundcloud.com>
2016-12-13 20:06:38 -0500

    switch some lines

--- SpringIntoLife.playground/Pages/Cool Dynamics.xcplaygroundpage/Contents.swift
+++ SpringIntoLife.playground/Pages/Cool Dynamics.xcplaygroundpage/Contents.swift
@@ -6,8 +6,8 @@ import PlaygroundSupport
 let containerView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 500, height: 500)))
 containerView.backgroundColor = UIColor.white
 
-PlaygroundPage.current.liveView = containerView
 PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = containerView
 
 let interactiveView = InteractiveView(frame: containerView.frame)
 containerView.addSubview(interactiveView)
